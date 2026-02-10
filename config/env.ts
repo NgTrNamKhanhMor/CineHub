c2d2d76d78f16cd7f62eed835cfe34ed679ec1e0
khanhntn <khanh.ntn@mor.com.vn>
2026-02-10 13:43:46 +0700

    update/env

--- config/env.ts
+++ config/env.ts
@@ -1,9 +1,9 @@
 export const API_CONFIG = {
-  TMDB_API_KEY: process.env.TMDB_API_KEY || '',
-  OMDB_API_KEY: process.env.OMDB_API_KEY || '',
-  TMDB_BASE_URL: process.env.TMDB_BASE_URL || '',
-  OMDB_BASE_URL: process.env.OMDB_BASE_URL || '',
-  TMDB_IMAGE_BASE_URL: process.env.TMDB_IMAGE_BASE_URL || '',
+  TMDB_API_KEY: process.env.EXPO_PUBLIC_TMDB_API_KEY || '',
+  OMDB_API_KEY: process.env.EXPO_PUBLIC_OMDB_API_KEY || '',
+  TMDB_BASE_URL: process.env.EXPO_PUBLIC_TMDB_BASE_URL || '',
+  OMDB_BASE_URL: process.env.EXPO_PUBLIC_OMDB_BASE_URL || '',
+  TMDB_IMAGE_BASE_URL: process.env.EXPO_PUBLIC_TMDB_IMAGE_BASE_URL || '',
   ENABLE_OMDB: true,       
   ENABLE_LETTERBOXD: true,
 };
